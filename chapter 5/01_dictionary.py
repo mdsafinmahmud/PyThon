068a850d8014f70b6e704fb8891877c0c87d1b48
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-03-25 21:19:57 +0600

    updated

---
+++ chapter 5/01_dictionary.py
@@ -0,0 +1,17 @@
+marks = {
+    "Safin Mahmud": 100,
+    "Samiul Islam": 90,
+    "Shakib Al Hasan": 95,
+    0: "Safin Mahmud",
+}
+print(marks, type(marks))
+
+print(marks.items())
+
+print(marks.keys())
+
+print(marks.values())
+
+marks.update({"Samiul Islam": 95, "Safin Mahmud": 99, "Yatra Rahman": 98})
+# print(marks)
+print(marks.get("Safin Mahmud", "Not found"))
