948a2f9eac10c51c5ff92697c61a1e6f66736969
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-03-25 17:26:39 +0600

    Update 02_detatypes.py

--- chapter 2/02_detatypes.py
+++ chapter 2/02_detatypes.py
@@ -1,9 +1,6 @@
-a = 1
-# a is an integer
-b = 244
-# b is a floating point number
+a = 10  # a is an integer
+b = 244  # b is a floating point number
 c = "safin\n"  # c is a string
-d = True # d is a boolean
-e = None
-# e is a NoneType variable
+d = True  # d is a boolean
+e = None  # e is a NoneType variable
 print((a + b), c, d, e)
