30ef762e8fc466bdfb8c8507e70698956e9a5f1e
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-03-25 21:52:45 +0600

    create new

---
+++ chapter 5/sets.py
@@ -0,0 +1,3 @@
+s = {2, 3, 4, 5, 5, 5, 5, 5}
+e = set()  # use empty set s=()
+print(s)
---
+++ chapter 5/sets_methods.py
@@ -0,0 +1,6 @@
+s = {2, 3, 4, 5, 5, 5, 5, 5, "harry"}
+print(s, type(s))
+s.add(56)
+print(s, type(s))
+s.remove(5)
+print(s)
