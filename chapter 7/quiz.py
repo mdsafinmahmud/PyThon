24e469b74391ff2e77dd0731a52375752182fa3e
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-03-27 16:35:05 +0600

    python files

---
+++ chapter 7/quiz.py
@@ -0,0 +1,6 @@
+my_list = [10, 20, 30, 40, 50, 120, 1292, 1209]
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
---
+++ chapter 7/while_loop.py
@@ -0,0 +1,4 @@
+i = 0
+while i < 10:
+    print("Happy Birthday!")
+    i = i + 1
