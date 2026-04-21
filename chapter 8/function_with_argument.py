f1fd4e1746d272e9d80a9c6c43583c6c26d40a4f
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-04-21 22:42:36 +0600

    Update function_with_argument.py

--- chapter 8/function_with_argument.py
+++ chapter 8/function_with_argument.py
@@ -1,5 +1,10 @@
-def goodDay(name):
+def goodDay(name, ending):
     print("Good day," + name)
+    print(ending)
 
 
-goodDay("Safin")
+goodDay("Safin", "Thanks")
+goodDay("Saffa", "Thanks")
+goodDay("Shanto", "Thanks")
+goodDay("Hridoy", "Thanks")
+goodDay("Shafuda", "Thanks")
