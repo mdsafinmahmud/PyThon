184622535297c265ea8b9ee8e492375e2c602566
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-04-21 20:24:42 +0600

    Create function.py

---
+++ chapter 8/function.py
@@ -0,0 +1,30 @@
+# a = int(input("Enter a number"))
+# b = int(input("Enter a number"))
+# c = int(input("Enter a number"))
+# avg = (a + b + c) / 3
+# print("Average=", avg)
+
+# a = int(input("Enter a number"))
+# b = int(input("Enter a number"))
+# c = int(input("Enter a number"))
+# avg = (a + b + c) / 3
+# print("Average=", avg)
+
+# a = int(input("Enter a number"))
+# b = int(input("Enter a number"))
+# c = int(input("Enter a number"))
+# avg = (a + b + c) / 3
+# print("Average=", avg)
+
+def avg():
+    a = int(input("Enter a number"))
+    b = int(input("Enter a number"))
+    c = int(input("Enter a number"))
+    avg = (a + b + c) / 3
+    print("Average=", avg)
+
+avg()
+avg()
+avg()
+avg()
+avg()
